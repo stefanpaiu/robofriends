688ecabe7f02b693b64beadb1d2c13cd0db43f1c
Stefan <stefan.paiu.0595@gmail.com>
2023-08-11 17:43:15 +0200

    Added Redux. Fetch not working. Needs tidying

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,83 @@
-import React, { useState, useEffect} from "react";
+import React, {useState, useEffect, Component} from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
-function App () {
-  const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState('')
+import { setSearchField, requestRobots } from "../actions";
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => {
-        return response.json();
-      })
-      .then((users) => {
-        setRobots(users);
-      });
-  }, [])
-
-  // Sets the state - changes
-  const onSearchChange = (event) => {
-    setSearchfield(event.target.value);
-  };
-
-  const filteredRobots = robots.filter((robot) => {
-    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-  });
-
-  return !robots.length ? (
-    <h1>Loading</h1>
-  ) : (
-    <div className="tc">
-      <h1 className="f1">RoboFriends</h1>
-      <SearchBox searchChange={onSearchChange} />
-      <Scroll>
-        <ErrorBoundry>
-          <CardList robots={filteredRobots} />
-        </ErrorBoundry>
-      </Scroll>
-    </div>
-  );
+// The state I need to listen to and send down to props.
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
 }
 
-export default App;
+// What props I should listen to that are actions and need to be dispatched.
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => {
+      dispatch(setSearchField(event.target.value))
+    },
+    onRequestRobots: () => {
+      requestRobots(dispatch)
+    }
+  }
+}
+
+class App extends Component {
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
+
+  // With Hooks
+  // const [robots, setRobots] = useState([]);
+  // const [searchfield, setSearchfield] = useState('')
+  // With Redux
+
+  render() {
+    const {searchField, onSearchChange, robots, isPending} = this.props;
+// With Hooks
+    //useEffect(() => {
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((users) => {
+    //     setRobots(users);
+    //   });
+
+    // }, [])
+
+
+    // Sets the state - changes
+    // const onSearchChange = (event) => {
+    //   setSearchfield(event.target.value);
+    // };
+
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    return isPending ? (
+        <h1>Loading</h1>
+    ) : (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={onSearchChange}/>
+          <Scroll>
+            <ErrorBoundry>
+              <CardList robots={filteredRobots}/>
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware, combineReducers} from "redux";
+import {createLogger} from "redux-logger";
+import thunkMiddleware from 'redux-thunk';
 import "./index.css";
 import App from "./containers/App";
 import "tachyons";
 import reportWebVitals from "./reportWebVitals";
+import {searchRobots, requestRobots} from "./reducers";
+
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>);
 
 reportWebVitals();
